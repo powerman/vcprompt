883dae6c90e763b5153f7e080577d0717f48e912
Yuya Nishihara <yuya@tcha.org>
2010-04-18 19:05:58 +0900

    hg: show "tip" tag if working dir is tip
    This only supports RevlogNG format. (Mercurial >= 0.9 ?)

--- src/hg.c
+++ src/hg.c
@@ -1,5 +1,14 @@
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#if defined __BEOS__ && !defined __HAIKU__
+#include <ByteOrder.h>
+#else
+#include <arpa/inet.h>
+#endif
+
 #include "common.h"
 #include "hg.h"
 
@@ -18,6 +27,63 @@ static int sum_bytes(const unsigned char* data, int size)
         sum += data[i];
     }
     return sum;
+}
+
+static int is_revlog_inlined(FILE *f)
+{
+    const unsigned int REVLOGNGINLINEDATA = 1 << 16;
+    int revlog_ver;
+    size_t origpos, rlen;
+
+    origpos = ftell(f);
+    rlen = fread(&revlog_ver, sizeof(revlog_ver), 1, f);
+    fseek(f, origpos, SEEK_SET);
+
+    revlog_ver = ntohl(revlog_ver);
+    return (rlen == 1) ? revlog_ver & REVLOGNGINLINEDATA : 0;
+}
+
+static const char* get_tip_nodeid(char* dest)
+{
+    // only supports RevlogNG. See mercurial/parsers.c for details.
+    const char* REVLOG_FILENAME = ".hg/store/00changelog.i";
+    const size_t ENTRY_LEN = 64, COMP_LEN_OFS = 8, NODEID_OFS = 32;
+
+    char buf[ENTRY_LEN];
+    FILE* f;
+    int inlined;
+
+    f = fopen(REVLOG_FILENAME, "rb");
+    if (!f) {
+        debug("error opening '%s': %s", REVLOG_FILENAME, strerror(errno));
+        memset(dest, '\0', NODEID_LEN);
+        return dest;
+    }
+
+    inlined = is_revlog_inlined(f);
+
+    while (!feof(f)) {
+        size_t comp_len, rlen;
+
+        rlen = fread(buf, 1, ENTRY_LEN, f);
+        if (rlen == 0) break;
+        if (rlen != ENTRY_LEN) {
+            debug("error while reading '%s': incomplete entry (read = %d)",
+                  REVLOG_FILENAME, rlen);
+            memset(dest, '\0', NODEID_LEN);
+            break;
+        }
+
+        comp_len = ntohl(*((uint32_t *) (buf + COMP_LEN_OFS)));
+        memcpy(dest, buf + NODEID_OFS, NODEID_LEN);
+
+        if (inlined) fseek(f, comp_len, SEEK_CUR);
+    }
+
+    debug("read tip nodeid: %08x", ntohl(*((uint32_t *) dest)));
+
+    fclose(f);
+    return dest;
 }
 
 static size_t get_mq_patchname(char* str, const char* nodeid, size_t n)
@@ -63,6 +129,12 @@ static size_t put_nodeid(char* str, const char* nodeid)
         memcpy(p, buf, n); p += n;
         *p = ']'; ++p;
         *p = '\0';
+    } else {
+        get_tip_nodeid(buf);
+        if (memcmp(nodeid, buf, NODEID_LEN) == 0) {
+            strcpy(p, "[tip]");
+            p += 5;
+        }
     }
 
     return p - str;
