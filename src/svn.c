d0ccc7d989c03cba0759c9edf0258be896bd1a42
Greg Ward <greg@gerg.ca>
2013-03-04 17:37:25 -0500

    svn: simplify svn_get_info() by de-nesting an unnecessary "else" branch

--- src/svn.c
+++ src/svn.c
@@ -25,65 +25,69 @@ static result_t*
 svn_get_info(vccontext_t *context)
 {
     result_t *result = init_result();
+    FILE *fp = NULL;
     char buf[1024];
 
     if (!read_first_line(".svn/entries", buf, 1024)) {
-        debug("failed to read from .svn/entries: assuming not an svn repo");
+        debug("failed to read from .svn/entries: not an svn working copy");
         goto err;
     }
-    else {
-        FILE *fp;
-        fp = fopen(".svn/entries", "r");
-        char line[1024];
 
-        // Check the version
-        if (fgets(line, sizeof(line), fp)) {
-            if(isdigit(line[0])) {
-                // Custom file format (working copy created by svn >= 1.4)
+    fp = fopen(".svn/entries", "r");
+    if (!fp) {
+        debug("failed to open .svn/entries: not an svn working copy");
+        goto err;
+    }
+    char line[1024];
 
-                // Read and discard line 2 (name), 3 (entries kind)
-                if (fgets(line, sizeof(line), fp) == NULL ||
-                    fgets(line, sizeof(line), fp) == NULL) {
-                    debug("early EOF reading .svn/entries");
-                    fclose(fp);
-                    goto err;
-                }
+    // Check the version
+    if (fgets(line, sizeof(line), fp)) {
+        if(isdigit(line[0])) {
+            // Custom file format (working copy created by svn >= 1.4)
 
-                // Get the revision number
-                if (fgets(line, sizeof(line), fp)) {
-                    chop_newline(line);
-                    result->revision = strdup(line);
-                    debug("read a svn revision from .svn/entries: '%s'", line);
-                }
-                else {
-                    debug("early EOF: expected revision number");
-                    fclose(fp);
-                    goto err;
-                }
+            // Read and discard line 2 (name), 3 (entries kind)
+            if (fgets(line, sizeof(line), fp) == NULL ||
+                fgets(line, sizeof(line), fp) == NULL) {
+                debug("early EOF reading .svn/entries");
+                goto err;
+            }
+
+            // Get the revision number
+            if (fgets(line, sizeof(line), fp)) {
+                chop_newline(line);
+                result->revision = strdup(line);
+                debug("read a svn revision from .svn/entries: '%s'", line);
             }
             else {
-                // XML file format (working copy created by svn < 1.4)
-                char rev[100];
-                char *marker = "revision=";
-                char *p = NULL;
-                while (fgets(line, sizeof(line), fp))
-                    if ((p = strstr(line, marker)) != NULL)
-                        break;
-                if (p == NULL) {
-                    debug("no 'revision=' line found in .svn/entries");
-                    goto err;
-                }
-                if (sscanf(p, " %*[^\"]\"%[0-9]\"", rev) == 1) {
-                    result_set_revision(result, rev, -1);
-                    debug("read svn revision from .svn/entries: '%s'", rev);
-                }
+                debug("early EOF: expected revision number");
+                goto err;
             }
         }
-        fclose(fp);
+        else {
+            // XML file format (working copy created by svn < 1.4)
+            char rev[100];
+            char *marker = "revision=";
+            char *p = NULL;
+            while (fgets(line, sizeof(line), fp))
+                if ((p = strstr(line, marker)) != NULL)
+                    break;
+            if (p == NULL) {
+                debug("no 'revision=' line found in .svn/entries");
+                goto err;
+            }
+            if (sscanf(p, " %*[^\"]\"%[0-9]\"", rev) == 1) {
+                result_set_revision(result, rev, -1);
+                debug("read svn revision from .svn/entries: '%s'", rev);
+            }
+        }
     }
+    fclose(fp);
     return result;
+
  err:
     free(result);
+    if (fp)
+        fclose(fp);
     return NULL;
 }
 
